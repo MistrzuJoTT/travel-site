3349cb5cfe21d39bae42719d0a1ed607a5f32551
MistrzuJoTT <jamana113@gmail.com>
2017-11-01 01:31:37 +0100

    webpack

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -18,6 +18,9 @@ gulp.task('watch', function() {
   watch('./app/assets/styles/**/*.css', function() {
     gulp.start('cssInject');
   });
+  watch('./app/assets/scripts/**/*.js', function() {
+    gulp.start('scriptsRefresh');
+  })
 
 });
 
@@ -25,3 +28,7 @@ gulp.task('cssInject', ['styles'], function() {
   return gulp.src('./app/temp/styles/styles.css')
     .pipe(browserSync.stream());
 });
+
+gulp.task('scriptsRefresh', ['scripts'],  function() {
+  browserSync.reload();
+})
